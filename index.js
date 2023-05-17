2a3ef6a75901e9d68fcb986242f051a3e50b2e6b
Tanmoy5992 <tanmoychakraborty105@gmail.com>
2023-05-18 00:59:23 +0530

    upload images removed

--- index.js
+++ index.js
@@ -26,131 +26,29 @@ app.post("/upload_files",upload.any(), uploadFiles);
 
 function uploadFiles(req, res) {
 
-    let addObj = []; 
-    const counter = req.body.counter;
-    const fileLength = req.files.length
+  let addObj = []; 
+  const counter = req.body.counter;
+  const fileLength = req.files.length
 
-    if(fileLength > 0){
-
-      for(let i=0 ; i < counter ; i++){
-
-        for(let y=0 ; y < fileLength ; y++){
-
-          if(req.files[y].fieldname == `file${i}`){
-
-            const newObj = compareArrayObject({name:req.body.text[i],document:req.files[y].path,isFile:'true',index:i},addObj)
-            
-            if(newObj.delete){
-
-              const indexOfObject = addObj.findIndex(object => {
-
-                return object.index === newObj.deleteIndex;
-                
-                });
-              addObj.splice(indexOfObject, 1)
-              addObj.push({name:req.body.text[i],document:req.files[y].path,isFile:'true',index:i})
-            
-            } else {
-              
-              if(newObj.newPush){
-              
-                addObj.push({name:req.body.text[i],document:req.files[y].path,isFile:'true',index:i})
-              
-              }
-            }
-          } else {
-
-            const newObj = compareArrayObject({name:req.body.text[i],document:'',isFile:'false',index:i},addObj)
-            
-            if(newObj.delete){
-              
-              const indexOfObject = addObj.findIndex(object => {
-
-                return object.index === newObj.deleteIndex;
-                
-                });
-              addObj.splice(indexOfObject, 1)
-              addObj.push({name:req.body.text[i],document:'',isFile:'false',index:i})
-            
-            } else {
-              
-              if(newObj.newPush){
-              
-                addObj.push({name:req.body.text[i],document:'',isFile:'false',index:i})
-              
-              }
-            
-            }
-          
+  for(let i=0; i<counter;i++ ){
+    var fileName=req.files.filter(obj => obj.fieldname == `file${i}`);
+    if (fileName.length > 0) {
+        console.log('file present'); 
+        addDataD = {
+            name:req.body.text[i],
+            document:fileName[0].filename
+          }                    
+        addObj.push(addDataD);
+    }else{
+        console.log("file not present");
+        addDataD = {
+            name:req.body.text[i]                        
           }
-        
-        }
-        
-      }
-
-    } else {
-      
-      for(let i=0 ; i < counter ; i++){
-      
-        addObj.push({name:req.body.text[i],document:'',isFile:'false',index:i})
-      
-      }
-    
+        addObj.push(addDataD);
     }
-
-    res.json({ addObj:addObj,message: "Successfully uploaded files" });
-}
-
-const compareArrayObject = (compareObject, existingObject) => {
-
-  if(existingObject.length == 0){
-    
-    return {delete:false,deleteIndex:0,newPush:true};
-  
-  } else {
-
-    const results = existingObject.filter(obj => {
-      
-      return obj.index == compareObject.index;
-    
-    });
-
-    if(results.length == 0){
-      
-      return {delete:false,deleteIndex:0,newPush:true};
-    
-    } else {
-      
-      if(compareObject.index == results[0].index){
-        
-        if(compareObject.isFile == results[0].isFile){
-          
-          return {delete:true,index:results[0].index}
-        
-        } else {
-          
-          if(results[0].isFile == 'true' && compareObject.isFile == 'false'){
-            
-            return {delete:false,deleteIndex:results[0].index,newPush:false}
-          
-          } else { 
-            
-            return {delete:true,deleteIndex:results[0].index}
-          
-          }          
-        
-        }
-      
-      } else {
-        
-        return {delete:false,deleteIndex:0,newPush:true};
-      
-      }
-    
-    }
-  
   }
 
+  res.json({ addObj:addObj,message: "Successfully uploaded files" });
 }
 
 app.listen(5000, () => {
